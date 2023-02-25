cf9a63b24041ec1c06f3882e96048c7a4c2d4fd8
Jakub Reroń <kubareron@gmail.com>
2023-02-25 21:41:54 +0100

    feat: get fastest mirrors before proceeding with pkglists install

--- setup/dotfiles_packages.sh
+++ setup/dotfiles_packages.sh
@@ -12,6 +12,10 @@ install_aur_helper() {
   fi
 }
 
+get_fastest_mirrors() {
+  sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist
+}
+
 install_pkglists() {
   if command -v $aur_helper &> /dev/null; then
     install_pkg - < "$pkglists_dir/$pkgtype/pacman.txt";
@@ -20,5 +24,6 @@ install_pkglists() {
 
 setup_dotfiles_packages() {
   install_aur_helper
+  get_fastest_mirrors
   install_pkglists
 }
