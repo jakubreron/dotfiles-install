adbf54fa85d6f2fd7a2d7d411e5794b094d31868
Jakub Reroń <kubareron@gmail.com>
2023-02-26 11:50:52 +0100

    fix: adds reflector to core packages

--- setup/basics.sh
+++ setup/basics.sh
@@ -7,7 +7,7 @@ update_system() {
 }
 
 setup_core_packages() {
-  for package in curl ca-certificates base-devel git ntp zsh rust laptop-detect stow; do
+  for package in curl ca-certificates base-devel git ntp zsh rust laptop-detect stow reflector; do
     install_pkg "$package"
   done
 }
