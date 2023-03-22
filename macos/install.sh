718af99e2c34d8503777eefbeb401cec57545b01
Jakub Reroń <kubareron@gmail.com>
2023-03-22 13:58:22 +0100

    docs(todo)

--- macos/install.sh
+++ macos/install.sh
@@ -3,6 +3,7 @@
 # TODO: add brew install
 # TODO: install findutils, node, yarn, fnm, exa, mcfly, aunpack, atool, fzf, gettext
 # TODO: after cloning pkglists, do cat brew.txt | xargs brew install
+# TODO: brew install koekeishiya/formulae/skhd; brew services start skhd
 
 . ../shared/variables.sh
 . ../shared/setup/basics.sh
