11b084d6df8c534ceff95bbb35899774715862d0
Jakub Reroń <kubareron@gmail.com>
2023-09-23 12:22:31 +0200

    fix: install keyd via package manager

--- arch/packages/installation/custom.sh
+++ arch/packages/installation/custom.sh
@@ -8,14 +8,17 @@ install_auto_cpufreq() {
       git clone https://github.com/AdnanHodzic/auto-cpufreq.git "$path"
       cd "$path" && sudo ./auto-cpufreq-installer
       sudo auto-cpufreq --install
+      rm -rf "$path"
+    fi
+
+    if command -v auto-cpufreq >/dev/null 2>&1; then
+      log_status "auto-cpufreq is already installed, performing the setup"️
+
       sudo systemctl enable --now auto-cpufreq.service
       sudo systemctl mask power-profiles-daemon.service
       curl -sL "https://raw.githubusercontent.com/AdnanHodzic/auto-cpufreq/master/auto-cpufreq.conf-example" | sudo tee /etc/auto-cpufreq.conf
       sudo sed -i '/^\[battery\]/,/^\[/s/^# scaling_min_freq = 800000/scaling_min_freq = 1200000/' /etc/auto-cpufreq.conf
       sudo systemctl restart auto-cpufreq.service
-      rm -rf "$path"
-    else
-      log_status "auto-cpufreq is already installed"️
     fi
   else
     log_status "No laptop detected, skipping auto_cpufreq installation"️
@@ -25,14 +28,15 @@ install_auto_cpufreq() {
 install_keyd() {
   if ! command -v keyd >/dev/null 2>&1; then
     log_progress "Installing and setting up keyd"
+    install_pkg keyd
+  fi
+
+  if command -v keyd >/dev/null 2>&1; then
+    log_status "keyd is already installed, performing the setup"️
+
     sudo usermod -aG keyd "$DI_USER"
-    path="$DI_GIT_CLONE_PATH/keyd"
-    git clone https://github.com/rvaiya/keyd "$path"
-    cd "$path" || exit
-    make && sudo make install
     sudo systemctl enable keyd --now
-    cd ..
-    rm -rf "$path"
+
     global_config_path="/etc/keyd/defaulf.conf"
     echo "[ids]
 *
@@ -87,8 +91,6 @@ f = C-right
 left = home
 # Move cursor to end of Line
 right = end" | sudo tee "$global_config_path"
-  else
-    log_status "keyd is already installed"️
   fi
 }
 
