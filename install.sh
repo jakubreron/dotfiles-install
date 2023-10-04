d4e0f495a25d652e3c0382e3e1f5efba26bcfcf8
Jakub Reroń <kubareron@gmail.com>
2023-10-04 12:29:03 +0200

    docs(todo): adds coretemp setup

--- install.sh
+++ install.sh
@@ -10,6 +10,24 @@ case "$OS" in
   Linux)
     # TODO: preinstall keepassxc addon (and maybe more essential addons) to the firefox
     # TODO: setup email
+    # TODO: investigate https://askubuntu.com/questions/463640/ubuntu-sensors-how-to-decrease-temperatures-threshold
+    # and maybe setup
+    # /etc/sensors.d/isa-coretemp
+    # chip "coretemp-isa-0000"
+    #    label temp1 "Package id 0"
+    #    compute temp1 @-15,@-2 
+    # 
+    #    label temp2 "Core 0"
+    #    compute temp2 @-15,@-2 
+    # 
+    #    label temp3 "Core 1"
+    #    compute temp3 @-15,@-2
+    #    
+    #    label temp4 "Core 2"
+    #    compute temp4 @-15,@-2
+    # 
+    #    label temp5 "Core 3"
+    #    compute temp5 @-15,@-2
 
     declare -x DI_USER="${DI_USER:-jakub}" 
     declare -x DI_PKG_TYPE="${DI_PKG_TYPE:-secondary}"
