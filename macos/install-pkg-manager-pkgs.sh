6ccbfc082d0a0b9a55891d72e63dee4c6c9b60fe
Jakub Reroń <kubareron@gmail.com>
2024-11-15 13:59:54 +0900

    refactor(brew): install from bundle

--- macos/install-pkg-manager-pkgs.sh
+++ macos/install-pkg-manager-pkgs.sh
@@ -7,10 +7,7 @@ update_system() {
 
 install_pkglists() {
   log_progress "Installing dotfiles packages"
-  cat "$DI_PKGLISTS_DIR/$DI_PKG_TYPE/brew.txt" | xargs brew install
-
-  log_progress "Installing dotfiles packages (casks)"
-  cat "$DI_PKGLISTS_DIR/$DI_PKG_TYPE/brew-casks.txt" | xargs brew install --cask
+  brew bundle install --file "$DI_PKGLISTS_DIR/$DI_PKG_TYPE/Brewfile"
 }
 
 update_system
