4bb690f0faf2ee405ddf6acaa004474f540dc1ab
Jakub Reroń <kubareron@gmail.com>
2025-11-26 15:42:46 +0900

    feat: firefox addons

--- after_install.sh
+++ after_install.sh
@@ -16,6 +16,27 @@ $HOME/.local/bin/sync/update-symlinks-all
 log_progress "Restarting firefox to enforce custom config"
 killall firefox
 
+log_progress "Opening Firefox Add-on pages for manual installation"
+declare -a addon_urls=(
+  "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/"
+  "https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/"
+  "https://addons.mozilla.org/en-US/firefox/addon/vimium-ff/"
+  "https://addons.mozilla.org/en-US/firefox/addon/youtube-recommended-videos/"
+  "https://addons.mozilla.org/en-US/firefox/addon/yomitan/"
+)
+case "$OS" in
+  Linux)
+    open_command="firefox"
+    ;;
+  Darwin)
+    open_command="open -a Firefox"
+    ;;
+esac
+for url in "${addon_urls[@]}"; do
+  firefox "$url" &
+  sleep 1 # Small delay to give Firefox a moment to open each tab/window
+done
+
 log_progress "Building bat cache"
 if command -v bat >/dev/null 2>&1; then
   bat cache --build
